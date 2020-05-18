5deed159580189827d27949d464968f167c5c392
alexia07 <alexiavernier@gmail.com>
2020-05-18 21:20:37 +0200

    Text

--- test.js
+++ test.js
@@ -1,28 +1,32 @@
 /********************************************************************
  * Name : test.js                                                   *
  * Description : Pryv Software engineer codding exercise 3          *
+ *                  Test file using supertest, mocha and chai       *
  * Creation date : 18.04.2020                                       *
  * Author : Alexia Vernier, contact@alexiavernier.com               *
  *                                                                  *
  *******************************************************************/
 
+//=======REQUIRES======
 const request = require('supertest');
 const express = require('express');
 const chai = require('chai');
  
+//=======GLOBAL VARIABLES=======
 const app = express();
 
 var expect = chai.expect;
 
-app.get('/user', function(req, res) {
+//=======TESTS=======
   res.status(200).json({ name: 'john' });
 });
 
+//---Hello World---
 describe('GET server /', function() {
   it('responds with "Hello World!"', function(done) {
     request('http://localhost:1234')
       .get('/')
-      .expect(200, done);
+      .expect(200, done)
   });
 });
 
